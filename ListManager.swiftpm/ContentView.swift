0515e44f5dffa3ef0b5ee4658ce6e694637e94a1
Ateto1202 <tonysu1204@gmail.com>
2023-12-30 22:13:53 +0800

    Create ListManager to the new branch Create the ListManager to manage the resource more ideal, the ListManager could add a independent resource and edit it.
    In addition, it could add tags to each recource but modify the text of content.

---
+++ ListManager.swiftpm/ContentView.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var sourceManager: SourceManager
+    
+    init() {
+        self.sourceManager = SourceManager()
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                if sourceManager.sources.count > 0 {
+                    displaySources()
+                }
+                OperatingButtonView()
+            }
+            .navigationTitle("My sources")
+        }
+    }
+    
+    func OperatingButtonView() -> some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button {
+                    sourceManager.addSource()
+                } label: {
+                    Text("+")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.accentColor)
+                        .clipShape(Circle())
+                        .padding(.trailing, 18)
+                }
+            }
+            .padding(.bottom, 18)
+        }
+    }
+    
+    func displaySources() -> some View {
+        List {
+            VStack {
+                ForEach(sourceManager.sources) { item in 
+                    NavigationLink {
+                        SourceContentView(source: item)
+                    } label: {
+                        Text(item.title)
+                            .padding()
+                    }
+                }
+            }
+        }
+    }
+}
