bccacb6c113b64fceb5b1e420e91b710e82d8ccb
Ateto1202 <tonysu1204@gmail.com>
2024-01-01 11:49:21 +0800

    Add OperatingButton to ContentView of SwipeViewCell

--- ContentView.swift
+++ ContentView.swift
@@ -2,9 +2,35 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var viewController = ViewControllerRepresentable()
+    
     var body: some View {
-        HStack {
-            ViewControllerRepresentable()
+        VStack {
+            ZStack {
+                viewController
+                OperatingButtonView()
+            }
+        }
+    }
+    
+    func OperatingButtonView() -> some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button {
+                    viewController.addData()
+                } label: {
+                    Text("+")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.accentColor)
+                        .clipShape(Circle())
+                        .padding(.trailing, 18)
+                }
+                .padding(.bottom, 18)
+            }
         }
     }
 }
